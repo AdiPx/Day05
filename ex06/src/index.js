22670a4f694a6d7a9907169312a6f7495f3216f4
Adipx <adipasic86@gmail.com>
2021-03-12 22:27:44 +0100

    .

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // Only change code below this line
+    var myName = "Arena";
+    var myString = "My name is " + myName + ", and I am well!" // myString is now "My name is Arena, and I am well!""
+    // Only change code above this line
+   
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code below this line
+    var myString = "Mistral is ";
+    var someAdjective = "the best!";
+    myString += someAdjective; // myString is now "Mistral is the best!"
+    // Only change code above this line
+   
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
